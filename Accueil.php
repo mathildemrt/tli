1c1a7662562b648d0af6b2f7f965936df6eac2a6
leo cornillon <leo cornillon>
2016-02-08 12:06:08 +0100

    leo pas content

---
+++ Accueil.php
@@ -0,0 +1,14 @@
+<head>
+	<title>Page d'acceuil</title>
+	<link href="Acceuil.css" rel="stylesheet" type="text/css"/>
+</head>
+<body id="PagedAcceuil">
+  <form oninput="" method="get" action="" id="formulaire">
+    <div class="" id="barre_recherche">
+    <li>
+    <input id="Search"  name="Search" tabindex="-1" type="text" placeholder="Rechercher sur le site" required> 
+    <input type="submit" value="Rechercher" /> 
+    </li>
+    </div>
+  </form>
+</body>
